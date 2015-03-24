1f14225485377c6781853faa85ad22df1b5e9d9a
Jobling <tiago.jobling@gmail.com>
2015-03-24 02:37:49 +0000

    Querido diário,
    Os nós agora já falam melhor connosco, e dizem quem fala connosco.
    
    Estou feliz...

--- ddt.c
+++ ddt.c
@@ -65,18 +65,21 @@ int main(int argc, char ** argv){
 				exit(0);
 			}
 			n = read(fd_aux, buffer, _SIZE_MAX_);
-			printf("%s", buffer);
+			printf("Someone said: %s", buffer);
 			err = switch_listen(buffer, fd_aux, &self);
 			memset((void *) buffer, (int) '\0', _SIZE_MAX_);
 		}			
 		
 		if (FD_ISSET(self.fd.predi, &rfds)){
-
+			n = read(self.fd.succi, buffer, _SIZE_MAX_);
+			printf("Predi sent: %s", buffer);
+			err = switch_listen(buffer, -1, &self);
+			memset((void *) buffer, (int) '\0', _SIZE_MAX_); 
 		}
 		
 		if (FD_ISSET(self.fd.succi, &rfds)){
 			n = read(self.fd.succi, buffer, _SIZE_MAX_);
-			printf("%s", buffer);
+			printf("Succi sent: %s", buffer);
 			err = switch_listen(buffer, -1, &self);
 			memset((void *) buffer, (int) '\0', _SIZE_MAX_); 			
 		}
